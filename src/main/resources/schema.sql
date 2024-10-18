2e0dc60111133eb4f1e1a3eb3473d62ed6a3ed30
wollman19 <wollmanc@csp.edu>
2024-10-18 09:20:14 -0500

    changing sql file and removing properties

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS payment (
 CREATE TABLE IF NOT EXISTS user (
                                     user_id INT AUTO_INCREMENT PRIMARY KEY,
                                     user_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                    emailaddress varchar(100),
+                                    user_emailaddress varchar(100),
                                     user_phone varchar(10),
                                     user_fname varchar(30),
                                     user_lname varchar(30),
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS user (
                                     user_zip varchar(5),
                                     parent_id int,
                                     sitter_id int,
-                                    UNIQUE (emailaddress)
+                                    UNIQUE (user_emailaddress)
 ) AUTO_INCREMENT=1000;
 CREATE TABLE IF NOT EXISTS chat (
                                     chat_id INT AUTO_INCREMENT PRIMARY KEY
