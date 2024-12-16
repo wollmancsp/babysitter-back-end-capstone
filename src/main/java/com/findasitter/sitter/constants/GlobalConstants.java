f8f9e24c505c1b8357d4d10fb43d0dff265f4f0f
PhillipB <null>
2024-12-15 19:01:18 -0600

    Fix

--- src/main/java/com/findasitter/sitter/constants/GlobalConstants.java
+++ src/main/java/com/findasitter/sitter/constants/GlobalConstants.java
@@ -3,6 +3,6 @@ package com.findasitter.sitter.constants;
 public class GlobalConstants {
     public static final String FRONT_END_PORT = "http://localhost:4200";
 
-    //Ex: C:\\Users\\Username\\Documents\\target\\
-    public static final String IDE_BASE_PATH = "C:\\Users\\Phillip\\Downloads\\babysitter-back-end-capstone\\target\\profilePicture\\";
+    //Ex: C:\Users\Username\Documents\target\profilePicture\
+    public static final String IDE_BASE_PATH = "C:\\Users\\Username\\Documents\\target\\profilePicture\\";
 }
